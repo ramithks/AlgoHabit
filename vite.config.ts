9ca520021c93c1331a1d85cb401acda85bd15b7e
Ramith Zolnoi <ramith.ks@zolnoi.com>
2025-08-09 00:25:49 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AlgoHabit/', // Set base for GitHub Pages
+  base: "/AlgoHabit/", // Set base for GitHub Pages
 });
